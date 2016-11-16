ba8f42ccf678ff68fdedaee6d3386746e9919c82
Mark Rivers <rivers@cars.uchicago.edu>
2016-11-15 22:27:47 -0600

    Added NDSA_Stride and NDSA_FillValue parameters

--- NDDriverStdArraysApp/src/NDDriverStdArrays.h
+++ NDDriverStdArraysApp/src/NDDriverStdArrays.h
@@ -42,6 +42,8 @@ protected:
     int NDSA_AppendMode_;
     int NDSA_NumElements_;
     int NDSA_NextElement_;
+    int NDSA_Stride_;
+    int NDSA_FillValue_;
     int NDSA_NewArray_;
     int NDSA_ArrayComplete_;
     int NDSA_ArrayData_;
@@ -49,10 +51,13 @@ protected:
 
 private:
     template <typename epicsType> asynStatus writeXXXArray(asynUser *pasynUser, void *pValue, size_t nElements);
-    template <typename epicsType, typename NDArrayType> void copyBuffer(size_t nextElement, void *pValue, size_t nElements);
+    template <typename epicsType, typename NDArrayType> void copyBuffer(size_t nextElement, size_t stride, void *pValue, size_t nElements);
+    template <typename NDArrayType> void fillBuffer(double fillValueDouble);
     void doCallbacks();
     void setArrayComplete();
     size_t arrayDimensions_[ND_ARRAY_MAX_DIMS];
+    NDArrayInfo arrayInfo_;
+    size_t dimProd_[ND_ARRAY_MAX_DIMS];
 };
 
 #define NDSA_CallbackModeString             "NDSA_CALLBACK_MODE"               /* (asynInt32,        r/w) Every update, when complete           */
@@ -60,6 +65,8 @@ private:
 #define NDSA_AppendModeString               "NDSA_APPEND_MODE"                 /* (asynInt32,        r/w) Enable or disable                     */
 #define NDSA_NumElementsString              "NDSA_NUM_ELEMENTS"                /* (asynInt32,        r/o) Number of elements currently in array */
 #define NDSA_NextElementString              "NDSA_NEXT_ELEMENT"                /* (asynInt32,        r/w) Next element to write to in array     */
+#define NDSA_StrideString                   "NDSA_STRIDE"                      /* (asynInt32,        r/w) Stride between elements in array      */
+#define NDSA_FillValueString                "NDSA_FILL_VALUE"                  /* (asynFloat64,      r/w) Fill value in append mode             */
 #define NDSA_NewArrayString                 "NDSA_NEW_ARRAY"                   /* (asynInt32,        r/o) Start a new array in append mode      */
 #define NDSA_ArrayCompleteString            "NDSA_ARRAY_COMPLETE"              /* (asynInt32,        r/o) Array is complete in append mode      */
 #define NDSA_ArrayDataString                "NDSA_ARRAY_DATA"                  /* (asynXXXArray,     r/o) Array data                            */
