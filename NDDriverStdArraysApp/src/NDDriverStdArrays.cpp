0e635f8c530296ad0cf6653cc2ad2182f4863da4
James Souter <james.souter@diamond.ac.uk>
2023-11-16 09:07:19 +0000

    base frame timestamp off epicsTS

--- NDDriverStdArraysApp/src/NDDriverStdArrays.cpp
+++ NDDriverStdArraysApp/src/NDDriverStdArrays.cpp
@@ -310,7 +310,6 @@ void NDDriverStdArrays::setArrayComplete()
 void NDDriverStdArrays::doCallbacks()
 {
     NDArray *pArray = this->pArrays[0];
-    epicsTimeStamp startTime;
     int imageCounter;
 
     if (!pArray) return;
@@ -321,9 +320,8 @@ void NDDriverStdArrays::doCallbacks()
 
     /* Put the frame number and timestamp into the NDArray */
     pArray->uniqueId = imageCounter;
-    epicsTimeGetCurrent(&startTime);
-    pArray->timeStamp = startTime.secPastEpoch+startTime.nsec/1.e9;
     updateTimeStamp(&pArray->epicsTS);
+    pArray->timeStamp = pArray->epicsTS.secPastEpoch + pArray->epicsTS.nsec/1.e9;
     this->getAttributes(pArray->pAttributeList);
     doCallbacksGenericPointer(pArray, NDArrayData, 0);
 }
